32f2ce1721bec17c95a75d785b1dec83d3b4355c
Hai Carroll <65102355+wanghua-lei@users.noreply.github.com>
2024-07-25 15:04:08 +0800

    Update pretrained.py

--- stable_audio_tools/models/pretrained.py
+++ stable_audio_tools/models/pretrained.py
@@ -23,3 +23,31 @@ def get_pretrained_model(name: str):
     model.load_state_dict(load_ckpt_state_dict(model_ckpt_path))
 
     return model, model_config
+
+def get_pretrained_model_my(name: str):
+    
+    # model_config_path = hf_hub_download(name, filename="model_config.json", repo_type='model')
+    model_config_path = 'stable_audio_tools/configs/model_configs/txt2audio/stable_audio_1_0_dac.json'
+    with open(model_config_path) as f:
+        model_config = json.load(f)
+
+    model = create_model_from_config(model_config)
+
+    # Try to download the model.safetensors file first, if it doesn't exist, download the model.ckpt file
+    # try:
+    #     model_ckpt_path = hf_hub_download(name, filename="model.safetensors", repo_type='model')
+    # except Exception as e:
+    #     model_ckpt_path = hf_hub_download(name, filename="model.ckpt", repo_type='model')
+    model_ckpt_path = name
+    state_dict = load_ckpt_state_dict(model_ckpt_path)
+    new_state_dict ={}
+    for key in state_dict:
+        if key.startswith('diffusion.'):
+            new_key = key[10:]
+            new_state_dict[new_key] = state_dict[key]
+
+    model.load_state_dict(new_state_dict)
+    # ckpt = load_ckpt_state_dict(model_ckpt_path)
+    # for key in new_state_dict.keys():
+    #     print(key)
+    return model, model_config
